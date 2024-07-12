7898c499e8adb3dc8b432396e86994c7cf28a2f1
SARVESHYOGI <140194642+SARVESHYOGI@users.noreply.github.com>
2024-07-13 00:28:45 +0530

    Update axios.js

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://tinder-clone-chi.vercel.app/'
-    // baseURL: 'http://localhost:8000/'
+    // baseURL: 'https://tinder-clone-chi.vercel.app/'
+    baseURL: 'http://localhost:8000/'
 });
 
 export default instance;
